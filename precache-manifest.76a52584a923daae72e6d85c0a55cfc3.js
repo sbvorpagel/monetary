b45157d5068b0daf49168955ef5a3e6ef7f7c247
sbvorpagel <sbvorpagel@gmail.com>
2019-05-09 18:37:38 -0300

    Updates

---
+++ precache-manifest.76a52584a923daae72e6d85c0a55cfc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0d6168658c3b5099d0938ce86c84fb",
+    "url": "/monetary/index.html"
+  },
+  {
+    "revision": "83196a92e9a4dcd6612c",
+    "url": "/monetary/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/monetary/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "83196a92e9a4dcd6612c",
+    "url": "/monetary/static/js/main.dd0a848a.chunk.js"
+  },
+  {
+    "revision": "d99c075db5d3a2207a51",
+    "url": "/monetary/static/js/runtime~main.65a67d88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monetary/static/media/logo.5d5d9eef.svg"
+  }
+]);
